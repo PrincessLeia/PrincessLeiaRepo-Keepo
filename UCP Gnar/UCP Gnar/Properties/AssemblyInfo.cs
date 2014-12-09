796f72808c53e967f56a4303c324d92f97ad7797
PrincessLeia <jango007@gmx.de>
2014-12-09 18:07:55 +0100

    UCP Gnar

--- UCP Gnar/UCP Gnar/Properties/AssemblyInfo.cs
+++ UCP Gnar/UCP Gnar/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("xSLx-Orbwalker")]
+[assembly: AssemblyTitle("UCP Gnar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("xSLx-Orbwalker")]
-[assembly: AssemblyCopyright("Copyright © xSLx 2014")]
+[assembly: AssemblyProduct("UCP Gnar")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5dd8cf7d-c0aa-4324-ab5b-47b64bcbd1c9")]
+[assembly: Guid("07bb8b35-11f2-4eda-96b4-af932d7ba861")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
